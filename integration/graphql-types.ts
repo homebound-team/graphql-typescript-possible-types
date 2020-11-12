f0716416981fc745bc1ec9df76113fc4a293610d
Stephen Haberman <shaberman@homebound.com>
2020-11-12 16:40:40 -0600

    Initial commit.

---
+++ integration/graphql-types.ts
@@ -0,0 +1,3 @@
+export const possibleTypes = {
+  HasName: ["Author", "Book"],
+};
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { GraphQLObjectType } from "graphql";
+import { code } from "ts-poet";
+import { PluginFunction, Types } from "@graphql-codegen/plugin-helpers";
+import PluginOutput = Types.PluginOutput;
+
+/** Generates a `possibleTypes` config object for apollo. */
+export const plugin: PluginFunction<{}> = async schema => {
+  // Create a map of interface -> implementing types
+  const interfaceImpls: Record<string, string[]> = {};
+  Object.values(schema.getTypeMap()).forEach(type => {
+    if (type instanceof GraphQLObjectType) {
+      for (const i of type.getInterfaces()) {
+        if (interfaceImpls[i.name] === undefined) {
+          interfaceImpls[i.name] = [];
+        }
+        interfaceImpls[i.name].push(type.name);
+      }
+    }
+  });
+
+  const content = await code`
+    export const possibleTypes = {
+      ${Object.entries(interfaceImpls).map(([name, impls]) => {
+        return `${name}: [${impls.map(n => `"${n}"`).join(", ")}],`;
+      })}
+    };
+  `.toStringWithImports();
+  return { content } as PluginOutput;
+};
