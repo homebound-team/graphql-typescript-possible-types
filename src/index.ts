a5e440d23dbc74ec199932dc6b5dc5bf25f0b1ec
Stephen Haberman <shaberman@homebound.com>
2022-08-26 17:39:26 -0500

    Bump cimg.

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { code } from "ts-poet";
 import PluginOutput = Types.PluginOutput;
 
 /** Generates a `possibleTypes` config object for apollo. */
-export const plugin: PluginFunction<{}> = async schema => {
+export const plugin: PluginFunction<{}> = async (schema) => {
   // Create a map of interface -> implementing types
   const interfaceImpls: Record<string, string[]> = {};
-  Object.values(schema.getTypeMap()).forEach(type => {
+  Object.values(schema.getTypeMap()).forEach((type) => {
     if (type instanceof GraphQLObjectType) {
       for (const i of type.getInterfaces()) {
         if (interfaceImpls[i.name] === undefined) {
@@ -21,7 +21,7 @@ export const plugin: PluginFunction<{}> = async schema => {
   const content = await code`
     export const possibleTypes = {
       ${Object.entries(interfaceImpls).map(([name, impls]) => {
-        return `${name}: [${impls.map(n => `"${n}"`).join(", ")}],`;
+        return `${name}: [${impls.map((n) => `"${n}"`).join(", ")}],`;
       })}
     } as const;
   `.toStringWithImports();
